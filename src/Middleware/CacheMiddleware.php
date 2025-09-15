da53fdcc223505403cde842ab46853ff182221f7
Alexander Dmitriev <admitriev@optimalgroup.ru>
2025-09-15 17:16:08 +0300

    feat: add X-Cache-Status header for API routes

--- src/Middleware/CacheMiddleware.php
+++ src/Middleware/CacheMiddleware.php
@@ -7,28 +7,32 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Middleware\RoutingMiddleware;
 use Slim\Psr7\Request;
+use Slim\Psr7\Response;
 
 class CacheMiddleware
 {
     /** @var Request $request */
     public function __invoke($request, $handler)
     {
-        if ($request->getMethod() == 'GET') {
+        $fromCache = false;
+
+        if ($request->getMethod() === 'GET') {
             $cache = new PhpCache($request);
             $result = $cache->init();
             if (!$result) {
-                /** @var \Slim\Psr7\Response $response */
+                /** @var Response $response */
                 $response = $handler->handle($request);
                 $body = json_decode($response->getBody(), true);
                 $cache->cache($body);
             } else {
-                $response = new \Slim\Psr7\Response();
+                $response = new Response();
                 $body = $response->getBody();
                 $body->write(json_encode($result, JSON_HEX_QUOT | JSON_HEX_TAG));
                 $response->withBody($body);
+                $fromCache = true;
             }
         }
 
-        return $response;
+        return $response->withHeader('X-Cache-Status', $fromCache ? 'hit' : 'miss');
     }
 }
